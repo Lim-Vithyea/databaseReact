a29b7fd4ceed376aab2475ab0ddeb62cc8bad4dc
Doem Samaun <samoun08092004@gmail.com>
2025-01-06 10:17:29 +0700

    chart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import UserForm from './components/UserForm';
 import StudentList from './components/StudentList';
-
+import Chart from './components/Chart';
 function App() {
   // const [reload, setReload] = useState(false);
   // const handleUserAdded = () => {
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
      <UserForm/>
      <StudentList/>
+     <Chart/>
     </div>
   );
 }
---
+++ src/components/Chart.js
@@ -0,0 +1,61 @@
+// src/components/StudentChart.js
+import React, { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function StudentChart() {
+  const [chartData, setChartData] = useState({ labels: [], datasets: [] });
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/tblstudent')
+      .then((response) => response.json())
+      .then((data) => {
+        const genderCounts = data.reduce((acc, tblstudent) => {
+          acc[tblstudent.gender] = (acc[tblstudent.gender] || 0) + 1;
+          return acc;
+        }, {});
+
+        const labels = Object.keys(genderCounts);
+        const dataset = Object.values(genderCounts);
+
+        setChartData({
+          labels: labels,
+          datasets: [
+            {
+              label: 'Number of Students by Gender',
+              data: dataset,
+              backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+              borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(153, 102, 255, 1)'],
+              borderWidth: 1
+            }
+          ]
+        });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Student Gender Distribution</h2>
+      <Bar data={chartData} />
+    </div>
+  );
+}
+
+export default StudentChart;
